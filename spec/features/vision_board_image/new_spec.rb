86b20575d21d2ef4785cc57921604ef1f221232f
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-05-26 15:17:06 -0700

    refactor flash

--- spec/features/vision_board_image/new_spec.rb
+++ spec/features/vision_board_image/new_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "New Vision Board Image", type: :feature do
   context "a User adds an image result to their visionboard" do
-    it "creates a VisionBoardImage for the User's VisionBoard" do
+    it "creates a VisionBoardImage" do
       user = create(:user)
       create(:vision_board, user_id: user.id)
       user2 = create(:user)
@@ -13,6 +13,20 @@ RSpec.describe "New Vision Board Image", type: :feature do
         visit new_vision_board_image_path(image_id: image.id)
         click_button "Add Image"
       }.to change {user.vision_board_images.count}.by(1)
+    end
+  end
+
+  context "a User adds an invalid image result to their visionboard" do
+    it "does not create a VisionBoardImage" do
+      user = create(:user)
+      create(:vision_board, user_id: user.id)
+      user2 = create(:user)
+
+      expect{
+        login_as(user)
+        visit new_vision_board_image_path(image_id: 1)
+        click_button "Add Image"
+      }.to_not change {user.vision_board_images.count}.by(1)
     end
   end
 end
