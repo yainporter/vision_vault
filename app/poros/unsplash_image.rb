ed939e25ea43772c46b50e179bc8a5cafeea9224
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-05-31 16:09:44 -0700

    feat: Add unsplash_id to VisionBoardImage and optional image_id

--- app/poros/unsplash_image.rb
+++ app/poros/unsplash_image.rb
@@ -1,6 +1,8 @@
 class UnsplashImage
-  attr_reader :title, :description, :alt_description, :regular_size, :full_size, :small
+  attr_reader :id, :title, :description, :alt_description, :regular_size, :full_size, :small, :created_at
   def initialize(data)
+    @id = data[:id]
+    @created_at = data[:created_at]
     @title = data[:slug]
     @description = data[:description]
     @alt_description = data[:alt_description]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_30_172345) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_31_230536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,9 +65,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_30_172345) do
 
   create_table "vision_board_images", force: :cascade do |t|
     t.bigint "vision_board_id", null: false
-    t.bigint "upload_id", null: false
+    t.bigint "upload_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "unsplash_id"
     t.index ["upload_id"], name: "index_vision_board_images_on_upload_id"
     t.index ["vision_board_id"], name: "index_vision_board_images_on_vision_board_id"
   end
