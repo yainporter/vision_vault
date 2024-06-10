e17f8688b1ceed55bdab2a49e7a05d7b895c2b4f
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-06-09 23:20:16 -0700

    feat: Add in tailwind styling for simple form

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,9 +12,13 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         ttcommons: ['tt-commons-pro', 'sans-serif'],
+        comma_base: ['common-base', 'sans-serif'],
         header: ['kudryashev-d-contrast', 'sans-serif']
       },
       colors: {
+        'amber': '#E0CE8F'
+      },
+      borderColor: {
         'amber': '#E0CE8F'
       }
     },
